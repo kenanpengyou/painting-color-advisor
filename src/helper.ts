7b9747ab20ac369520c9b875af7c93aca6b247aa
Zhu Liang (AA) <zhu.liang@cicc.com.cn>
2019-11-21 17:28:46 +0800

    setup revise painting area after image loaded

---
+++ src/helper.ts
@@ -0,0 +1,18 @@
+// https://developer.mozilla.org/zh-CN/docs/Web/API/Window/resize_event
+let throttle: Function = function (type: string, name: string, obj: HTMLElement | Window) {
+    obj = obj || window;
+    let running: boolean = false;
+    let func: EventListener = function () {
+        if (running) { return; }
+        running = true;
+        requestAnimationFrame(function() {
+            obj.dispatchEvent(new CustomEvent(name));
+            running = false;
+        });
+    };
+    obj.addEventListener(type, func);
+};
+
+export default {
+    throttle
+};
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,34 @@
 import "./index.scss";
 
 import Vue from "vue";
+import helper from "./helper";
+
+// base config for this application
+const baseConfig = {
+    edgeReserve: 100
+};
+
+let paintingCtx: CanvasRenderingContext2D;
 
 let totalVm: any = new Vue({
     el: "[data-total-container]",
     data: {
-        imageSrc: "",
+        imageReady: false,
+        painting: {
+            classObject: {
+                "-centered": true,
+                "-grab": false
+            },
+            mode: "normal"
+        },
+        canvasContainerStyle: {},
         dropBoxClass: {
             "-active": false
         }
+    },
+    mounted () {
+        paintingCtx = this.$refs.imageCanvas.getContext("2d");
+        bindEvents("init");
     },
     methods: {
         handleDragEnter () {
@@ -32,33 +52,132 @@ let totalVm: any = new Vue({
     }
 });
 
-document.addEventListener("paste", (e: ClipboardEvent) => {
-    e.preventDefault();
-    let clipboardData: DataTransfer = e.clipboardData;
+function bindEvents (flag: string): void {
 
-    if (clipboardData) {
-        let items: DataTransferItemList = clipboardData.items;
+    switch (flag) {
 
-        if (items && items.length > 0) {
-            
-            for (let i = 0; i < items.length; i++) {
-                let item: DataTransferItem = items[i];
+        // when the image has been loaded to canvas
+        case "imageReady": {
+            helper.throttle("resize", "optimizedResize", window);
 
-                if (item.type.includes("image")) {
-                    let blob: Blob = item.getAsFile();
-                    return loadImageBlobToCanvas(blob);
+            window.addEventListener("optimizedResize", () => {
+                console.log("[optimizedResize]");
+                adjustPaintingArea();
+            }, false);
+            document.addEventListener("keydown", (e: KeyboardEvent) => {
+
+                // key "space"
+                if (e.keyCode === 32) {
+                    e.preventDefault();
+                    toggleGrabMode("on");
                 }
-            }
+            }, false);
+            document.addEventListener("keyup", (e: KeyboardEvent) => {
+
+                if (e.keyCode === 32) {
+                    e.preventDefault();
+                    toggleGrabMode("off");
+                }
+            }, false);
+            break;
+        }
+
+        case "init":
+        default: {
+            document.addEventListener("paste", (e: ClipboardEvent) => {
+                e.preventDefault();
+                let clipboardData: DataTransfer = e.clipboardData;
+            
+                if (clipboardData) {
+                    let items: DataTransferItemList = clipboardData.items;
+            
+                    if (items && items.length > 0) {
+                        
+                        for (let i = 0; i < items.length; i++) {
+                            let item: DataTransferItem = items[i];
+            
+                            if (item.type.includes("image")) {
+                                let blob: Blob = item.getAsFile();
+                                return loadImageBlobToCanvas(blob);
+                            }
+                        }
+                    }
+                }
+            }, false);
         }
     }
-}, false);
+}
 
-
-function loadImageBlobToCanvas (imageBlob: Blob) {
+function loadImageBlobToCanvas (imageBlob: Blob): void {
 
     if (imageBlob) {
-        let url: String = URL.createObjectURL(imageBlob);
-        totalVm.imageSrc = url;
-    }
+        let image: HTMLImageElement = new Image();
+        let url: string = window.URL.createObjectURL(imageBlob);
+        
+        image.onload = function () {
+            totalVm.$refs.imageCanvas.width = image.width;
+            totalVm.$refs.imageCanvas.height = image.height;
+            totalVm.imageReady = true;
+            paintingCtx.drawImage(image, 0, 0);
+            bindEvents("imageReady");
+            adjustPaintingArea();
+        };
 
+        image.src = url;
+    }
+}
+
+function adjustPaintingArea (): void {
+    let canvas: HTMLCanvasElement = totalVm.$refs.imageCanvas;
+    let canvasWidth: number = canvas.width;
+    let canvasHeight: number = canvas.height;
+    let viewportWidth: number = window.innerWidth;
+    let viewportHeight: number = window.innerHeight;
+
+    // if larger than viewport
+    if (canvasWidth > viewportWidth || canvasHeight > viewportHeight) {
+        totalVm.painting.classObject["-centered"] = false;
+
+        let properContainerWidth: number = 2 * (viewportWidth - baseConfig.edgeReserve) + canvasWidth;
+        let properContainerHeight: number =  2 * (viewportHeight - baseConfig.edgeReserve) + canvasHeight;
+        totalVm.canvasContainerStyle = {
+            width: `${properContainerWidth}px`,
+            height: `${properContainerHeight}px`
+        };
+        // console.log("[adjustPaintingArea]properContainerWidth = ", properContainerWidth);
+
+        totalVm.$nextTick(function () {
+            this.$refs.paintingContainer.scrollLeft = (properContainerWidth - viewportWidth) / 2;
+            this.$refs.paintingContainer.scrollTop = (properContainerHeight - viewportHeight) / 2;
+        });
+
+    } else {
+        totalVm.painting.classObject["-centered"] = true;
+        totalVm.canvasContainerStyle = {};
+    }
+}
+
+function toggleGrabMode (direction: string): void {
+
+    switch (direction) {
+        case "on": {
+            totalVm.painting.classObject["-grab"] = true;
+            totalVm.painting.mode = "grab";
+
+            document.addEventListener("mouse", (e: KeyboardEvent) => {
+
+                if (e.keyCode === 32) {
+                    e.preventDefault();
+                    toggleGrabMode("off");
+                }
+            }, false);
+            break;
+        }
+
+        case "off":
+        default: {
+            totalVm.painting.classObject["-grab"] = false;
+            totalVm.painting.mode = "normal";
+        }   
+    }
 }
